4f80b126c29f11c930396f6f98532a36c1afb9f6
aman2151 <98450034+aman2151@users.noreply.github.com>
2023-01-22 16:05:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let timer1 = document.getElementById("timer1");
+let timer2 = document.getElementById("timer2");
+let value1 = 0
+let value2 = 0
+timer1.textContent = value1;
+timer2.textContent = value2;
+function increment1(){
+    timer1.textContent = value1+=1
+    
+}
+function increment2(){
+    timer1.textContent = value1+=2
+    
+}
+function increment3(){
+    timer1.textContent = value1+=3
+    
+}
+
+function increment4(){
+    timer2.textContent = value2+=1
+    
+}
+function increment5(){
+    timer2.textContent = value2+=2
+    
+}
+function increment6(){
+    timer2.textContent = value2+=3
+    
+}
